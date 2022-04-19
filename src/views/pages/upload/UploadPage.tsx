d5e5ad38cf1c0a2fe7ea42b12088f1837853b626
Venura <valw0002@student.monash.edu>
2022-04-20 08:59:30 +1000

    Added upload button, title and description

--- src/views/pages/upload/UploadPage.tsx
+++ src/views/pages/upload/UploadPage.tsx
@@ -1,9 +1,25 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
+import { Button, TextField, Input } from '@mui/material';
+import UploadIcon from '@mui/icons-material/Upload';
 
 export default function UploadPage() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
     return (
-        <div>
+        <>
             <h1>Upload data</h1>
-        </div>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <label htmlFor="contained-button-file">
+                    <Input inputProps={{ accept: '.csv, .json' }} type="file" />
+                    <Button variant="contained" component="span" startIcon={<UploadIcon />}>
+                        Upload
+                    </Button>
+                </label>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'column', rowGap: '1rem' }}>
+                <TextField onChange={(e) => setTitle(e.target.value)} required label="Title" variant="outlined" />
+                <TextField onChange={(e) => setDescription(e.target.value)} label="Description" variant="outlined" multiline rows={5} />
+            </div>
+        </>
     );
 }
