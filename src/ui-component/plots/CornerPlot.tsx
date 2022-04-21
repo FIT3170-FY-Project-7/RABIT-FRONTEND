0881e45640794e74bdcdf6cafb4139dd5a954f39
Daniel-W-230 <dwat005@student.monash.edu>
2022-04-21 11:08:37 +1000

    Add corner plot component

---
+++ src/ui-component/plots/CornerPlot.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// This component will hold Histogram plots and Contour plots. It is our base component for displaying all 2d plots
+// Should receive which parameters are being plotted from parent component (plots view) ../views/pages/plots
+// Should have logic for generating the right number of plots and putting them in the right places.
+
+function CornerPlot() {
+    return <div>CornerPlot</div>;
+}
+
+export default CornerPlot;
