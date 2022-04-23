b21f93dfe3bf7325d0f9ef559552fe89b2b9cc09
Bevan Lewis <blew0009@student.monash.edu>
2022-04-23 10:09:51 +1000

    Added Contour and Histogram plots

--- src/ui-component/plots/ContourPlot.tsx
+++ src/ui-component/plots/ContourPlot.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
 
 // Recieves 2 parameters
+type ContourPlotType = {
+    data: number[][];
 
-function ContourPlot() {
-    return <div>ContourPlot</div>;
+    layout: {
+        [k: string]: string | object | boolean;
+        
+        // The main title of the graph (optional)
+        title?: string;
+        xaxis?: {
+            [k: string]: string | boolean;
+
+            title?: string;
+            showticklabels?: boolean; // Determines if the xaxis values are shown or not
+        };
+        yaxis?: {
+            [k: string]: string | boolean;
+
+            title?: string;
+            showticklabels?: boolean; // Determines if the yaxis values are shown or not
+        };
+        
+        showscale: false;
+    };
+};
+
+function ContourPlot({ data, layout }: ContourPlotType) {
+    return (
+        <div>
+            <Plot
+                data={[
+                    {
+                        z: data,
+                        type: 'contour',
+                        contours: {
+                            // Makes the contour colours smoother
+                            coloring: 'heatmap'
+                        }
+                    }
+                ]}
+                layout={layout}
+            />
+        </div>
+    );
 }
 
 export default ContourPlot;
--- src/ui-component/plots/HistogramPlot.tsx
+++ src/ui-component/plots/HistogramPlot.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
 
 // Recieves 1 parameter
 
-function HistogramPlot() {
-    return <div>HistogramPlot</div>;
+type HistogramPlotType = {
+    [k: string]: object[] | object;
+
+    data: number[];
+    
+    // The main title of the graph (optional)
+    layout?: {
+        [k: string]: string | object | boolean;
+
+        title?: string;
+        xaxis?: {
+            [k: string]: string | boolean;
+
+            title?: string;
+            showticklabels?: boolean; // Determines if the xaxis values are shown or not
+        };
+        yaxis?: {
+            [k: string]: string | boolean;
+
+            title?: string;
+            showticklabels?: boolean; // Determines if the yaxis values are shown or not
+        };
+    };
+};
+
+function HistogramPlot({ data, layout }: HistogramPlotType) {
+    return (
+        <div>
+            <Plot
+                data={[
+                    {
+                        x: data,
+                        type: 'histogram'
+                    }
+                ]}
+                layout={layout}
+            />
+        </div>
+    );
 }
 
 export default HistogramPlot;
