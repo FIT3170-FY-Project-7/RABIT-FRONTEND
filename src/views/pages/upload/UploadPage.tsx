95b6ef46f43250b379d979f05c5503d4fa28dd80
Rohit <rraj0029@student.monash.edu>
2022-04-25 16:34:29 +1000

    Centred "Upload data" Title and upload buttons

--- src/views/pages/upload/UploadPage.tsx
+++ src/views/pages/upload/UploadPage.tsx
@@ -1,13 +1,16 @@
 ﻿import React, { useState } from 'react';
 import { Button, TextField, Input } from '@mui/material';
 import UploadIcon from '@mui/icons-material/Upload';
+import { Grid } from '@mui/material';
 
 export default function UploadPage() {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     return (
         <>
+        <Grid container direction="column" alignItems="center">
             <h1>Upload data</h1>
+        
             <div style={{ display: 'flex', flexDirection: 'column' }}>
                 <label htmlFor="contained-button-file">
                     <Input inputProps={{ accept: '.csv, .json' }} type="file" />
@@ -16,10 +19,12 @@ export default function UploadPage() {
                     </Button>
                 </label>
             </div>
+        </Grid>
             <div style={{ display: 'flex', flexDirection: 'column', rowGap: '1rem' }}>
                 <TextField onChange={(e) => setTitle(e.target.value)} required label="Title" variant="outlined" />
                 <TextField onChange={(e) => setDescription(e.target.value)} label="Description" variant="outlined" multiline rows={5} />
             </div>
+        
         </>
     );
 }
