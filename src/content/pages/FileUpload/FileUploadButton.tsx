3421067f6b216f40b2c5036653853f26913b07e3
Venura <valw0002@student.monash.edu>
2022-05-02 21:55:13 +1000

    Changing upload format

--- src/content/pages/FileUpload/FileUploadButton.tsx
+++ src/content/pages/FileUpload/FileUploadButton.tsx
@@ -1,28 +1,35 @@
-import UploadIcon from '@mui/icons-material/Upload';
-import { Button } from '@mui/material';
-import axios from 'axios';
-import { boolean } from 'yup';
+import UploadIcon from "@mui/icons-material/Upload";
+import { Button } from "@mui/material";
+import axios from "axios";
 
 interface FileSelect {
-    enableButton: boolean;
-    selectedFile: any;
+	enableButton: boolean;
+	selectedFile: any;
 }
 
-export default function FileSelectButton({ enableButton, selectedFile }: FileSelect) {
-    const handleSubmission = () => {
-        console.log(selectedFile);
-        const data = new FormData();
-        data.append('file', selectedFile);
-        console.log(data.getAll('file'));
-        //dev solution to test upload works
-        //run `npx nodemon ./server.tsx` in repo root to run local test server
-        axios.post('http://localhost:8000/uploads', data, {}).then((res) => {
-            console.log(res.statusText);
-        });
-    };
-    return (
-        <Button disabled={enableButton} variant="contained" startIcon={<UploadIcon />} onClick={handleSubmission}>
-            Upload
-        </Button>
-    );
+export default function FileSelectButton({
+	enableButton,
+	selectedFile,
+}: FileSelect) {
+	const handleSubmission = () => {
+		console.log(selectedFile);
+		const data = new FormData();
+		data.append("file", selectedFile);
+		console.log(data.getAll("file"));
+		//dev solution to test upload works
+		//run `npx nodemon ./server.tsx` in repo root to run local test server
+		axios.post("http://localhost:8000/uploads", data, {}).then((res) => {
+			console.log(res.statusText);
+		});
+	};
+	return (
+		<Button
+			disabled={enableButton}
+			variant="contained"
+			startIcon={<UploadIcon />}
+			onClick={handleSubmission}
+		>
+			Upload
+		</Button>
+	);
 }
