3d27d6195e1137f7101fec27e18ce5906766d125
Vincent <vmou0001@student.monash.edu>
2022-05-03 17:27:47 +1000

    datapage sidebar bug fix, about button moving when explore tab opened

--- src/layouts/DatapageLayout/Sidebar/index.tsx
+++ src/layouts/DatapageLayout/Sidebar/index.tsx
@@ -41,44 +41,31 @@ const TopSection = styled(Box)(
 )
 
 function Sidebar() {
-    const {sidebarToggle, toggleSidebar} = useContext(SidebarContext)
-    const closeSidebar = () => toggleSidebar()
-
     return (
         <> < Hidden lgDown > <SidebarWrapper>
             <Scrollbars autoHide>
-                <Stack alignItems = "center">
-                    <TopSection>
-                        <Logo/>
-                    </TopSection>
-                    <Input accept="*" id="import-data" multiple type="file" />
-                    <label htmlFor="import-data"> 
-                        <Button sx={{width:130}}
-                            startIcon={<UploadTwoToneIcon />}
-                            variant="contained"
-                            component="span"> 
-                        Import
-                        </Button>
-                    </label>
-                </Stack>
-                <SidebarMenu/> 
-                <Stack sx={{height:'70%'}} alignItems = "center" direction='column-reverse'>
+                <Box sx={{height:'50%'}}>
+                    <Stack alignItems = "center">
+                        <TopSection>
+                            <Logo/>
+                        </TopSection>
+                        <Input accept="*" id="import-data" multiple type="file" />
+                        <label htmlFor="import-data"> 
+                            <Button sx={{width:130}}
+                                startIcon={<UploadTwoToneIcon />}
+                                variant="contained"
+                                component="span"> 
+                            Import
+                            </Button>
+                        </label>
+                    </Stack>
+                <SidebarMenu/>
+                </Box> 
+                <Stack sx={{height:'45%'}} alignItems = "center" direction='column-reverse'>
                     <Button variant = "outlined" component="span"> About </Button>
                 </Stack>
             </Scrollbars>
         </SidebarWrapper>
-    </Hidden>
-    <Hidden lgUp>
-        <Drawer anchor='left' open={sidebarToggle} onClose={closeSidebar} variant='temporary' elevation={9}>
-            <SidebarWrapper>
-                <Scrollbars autoHide>
-                    <TopSection>
-                        <Logo/>
-                    </TopSection>
-                    <SidebarMenu/>
-                </Scrollbars>
-            </SidebarWrapper>
-        </Drawer>
     </Hidden>
 </>
     )
