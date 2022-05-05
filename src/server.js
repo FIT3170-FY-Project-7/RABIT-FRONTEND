9b83bc10058df70c6db28c69801d4cedcfddfd99
Rhac <rhac0001@student.monash.edu>
2022-05-05 14:53:24 +1000

    Fix an issue with the path and commented out examples

--- src/server.js
+++ src/server.js
@@ -2,22 +2,22 @@
 const app = express()
 const PORT = 3000;
 
-app.get("/test1", (req, res) => {
-    const test1 = {
-        "text" : "Hello World"
-    }
-    res.send(test1)
-})
+// app.get("/test1", (req, res) => {
+//     const test1 = {
+//         "text" : "Hello World"
+//     }
+//     res.send(test1)
+// })
 
-app.get("/test2/:id", (req, res) => {
-    const test2 = {
-        "text" : req.params.id
-    }
-    res.send(test2)
-})
+// app.get("/test2/:id", (req, res) => {
+//     const test2 = {
+//         "text" : req.params.id
+//     }
+//     res.send(test2)
+// })
 
-//const exampleRouter = require("./routes/example")
+app.use(express.static('public'));
 
-app.use("/example", exampleRouter)
+app.use(require("./routes/example"))
 
 app.listen(PORT, () => console.log("Listening on port: " + PORT))
