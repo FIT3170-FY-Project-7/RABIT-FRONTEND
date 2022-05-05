567e28a52eb887accae4516f34b7439ce83ea6cc
ddes0015 <ddes0015@student.monash.edu>
2022-05-05 11:56:05 +1000

    Convert Csv to Json
    Still unable to post json obj or string, will continue to work on post method

--- src/content/pages/FileUpload/FileUploadButton.tsx
+++ src/content/pages/FileUpload/FileUploadButton.tsx
@@ -1,6 +1,7 @@
 import UploadIcon from '@mui/icons-material/Upload';
 import { Button } from '@mui/material';
 import axios from 'axios';
+import csvToJson from 'csvtojson';
 
 interface FileUpload {
     enableButton: boolean;
@@ -9,15 +10,40 @@ interface FileUpload {
 
 export default function FileUploadButton({ enableButton, selectedFile }: FileUpload) {
     const handleSubmission = () => {
-        console.log(selectedFile);
-        const data = new FormData();
-        data.append('file', selectedFile);
-        console.log(data.getAll('file'));
-        //dev solution to test upload works
-        //run `npx nodemon ./server.tsx` in repo root to run local test server
-        axios.post('http://localhost:8000/uploads', data, {}).then((res) => {
-            console.log(res.statusText);
+
+
+        const csv = require('csv-parser')
+        const results = [];
+
+        selectedFile.text().then((csvStr) => {
+            csvToJson()
+            .fromString(csvStr)
+            .then((jsonObj)=>{
+            console.log(jsonObj);
+
+            var json = JSON.stringify(jsonObj);
+            const data = new FormData();
+            data.append('file', selectedFile);
+            console.log(data.getAll('file'));
+            //dev solution to test upload works
+            //run `npx nodemon ./server.tsx` in repo root to run local test server
+            axios.post('http://localhost:8000/uploads', data).then((res) => {
+                console.log(res.statusText);
+            });
+
+            });
+                    // console.log(selectedFile);
+
         });
+
+        
+
+        
+
+
+
+
+
     };
     return (
         <Button disabled={enableButton} variant="contained" startIcon={<UploadIcon />} onClick={handleSubmission}>
