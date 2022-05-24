780d11c37f4111bdc27d12a8b77c2646a7ac345e
Daniel-W-230 <dwat005@student.monash.edu>
2022-05-24 10:46:39 +1000

    feature/frontend-pages: Add background colour to plots

--- src/content/plots/ContourPlot.tsx
+++ src/content/plots/ContourPlot.tsx
@@ -2,31 +2,32 @@ import { useRef, useEffect } from "react";
 import ContD3 from "./d3/ContourD3";
 
 const ContourPlot = ({ x, y, layout }) => {
-	/* 
+    /* 
 
     Renders the contour plots using the D3 library. Calls create() in CountourD3 to render graph SVG.
 
     */
-	const elem = useRef(null);
+    const elem = useRef(null);
 
-	useEffect(() => {
-		ContD3.create(elem.current, layout, x, y);
-		return () => {
-			ContD3.destroy(elem.current);
-		};
-	});
+    useEffect(() => {
+        ContD3.create(elem.current, layout, x, y);
+        return () => {
+            ContD3.destroy(elem.current);
+        };
+    });
 
-	return (
-		<div
-			style={{
-				width: layout.width,
-				height: layout.height,
-				marginRight: layout.margin.horizontal,
-				marginTop: layout.margin.vertical,
-			}}
-			ref={elem}
-		></div>
-	);
+    return (
+        <div
+            style={{
+                width: layout.width,
+                height: layout.height,
+                marginRight: layout.margin.horizontal,
+                marginTop: layout.margin.vertical,
+                backgroundColor: "#CFE5FF",
+            }}
+            ref={elem}
+        ></div>
+    );
 };
 
 export default ContourPlot;
--- src/content/plots/HistogramPlot.tsx
+++ src/content/plots/HistogramPlot.tsx
@@ -2,31 +2,32 @@ import { useEffect, useRef } from "react";
 import HistD3 from "./d3/HistogramD3";
 
 const HistogramPlot = ({ x, layout }) => {
-	/* 
+    /* 
 
     Renders the histogram plots using the D3 library. Calls create() in HistogramD3 to render graph SVG.
 
     */
-	const elem = useRef(null);
+    const elem = useRef(null);
 
-	useEffect(() => {
-		HistD3.create(elem.current, layout, x);
-		return () => {
-			HistD3.destroy(elem.current);
-		};
-	});
+    useEffect(() => {
+        HistD3.create(elem.current, layout, x);
+        return () => {
+            HistD3.destroy(elem.current);
+        };
+    });
 
-	return (
-		<div
-			style={{
-				width: layout.width,
-				height: layout.height,
-				marginRight: layout.margin.horizontal,
-				marginTop: layout.margin.vertical,
-			}}
-			ref={elem}
-		></div>
-	);
+    return (
+        <div
+            style={{
+                width: layout.width,
+                height: layout.height,
+                marginRight: layout.margin.horizontal,
+                marginTop: layout.margin.vertical,
+                backgroundColor: "#CFE5FF",
+            }}
+            ref={elem}
+        ></div>
+    );
 };
 
 export default HistogramPlot;
