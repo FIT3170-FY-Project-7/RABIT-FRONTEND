dd9d356683c38263afdbec124fa739c3f3e43381
rraj0029 <rraj0029@student.monash.edu>
2022-08-30 23:49:37 +1000

    Commit to allow me to pull

--- src/content/pages/FileUpload/index.tsx
+++ src/content/pages/FileUpload/index.tsx
@@ -130,7 +130,7 @@ export default function UploadPage() {
 
                     {enableDeleteLast? (
                     <Button onClick={deleteSelectedFile}>Delete Last</Button>
-                    ) : null}
+                     ): null}
                 </Box>
                 <FileUploadButton
                     enableButton={!!selectedFiles?.length}
