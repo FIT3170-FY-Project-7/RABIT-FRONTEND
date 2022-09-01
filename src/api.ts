623d99356307f60acf985f941743f6aa32b9342a
JMSS-Unknown <31131631+JMSS-Unknown@users.noreply.github.com>
2022-09-01 15:03:43 +1000

    Update nectar backend api

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 let baseURL = 'http://localhost:8000/'
 
 if (process.env.backend === 'online') {
-  baseURL = 'https://backend.rabit2022.cloud.edu.au/'
+  baseURL = 'https://api.rabit2022.cloud.edu.au/'
 }
 
 const api = axios.create({
