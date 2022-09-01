5d03741e5802e7795fdc0bd8cf5e62a16bf06f15
JMSS-Unknown <31131631+JMSS-Unknown@users.noreply.github.com>
2022-09-01 18:36:04 +1000

    Fix incorrect id in navigation

--- src/content/pages/FileUpload/FileUploadButton.tsx
+++ src/content/pages/FileUpload/FileUploadButton.tsx
@@ -61,7 +61,7 @@ export default function FileUploadButton({
     })
     setIsProcessing(false)
 
-    navigate(`/visualise/${response.data.id}`)
+    navigate(`/visualise/${response.id}`)
   }
 
   if (isUploading) {
