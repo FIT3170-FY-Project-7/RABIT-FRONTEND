aa203a4fd4f7f2fa39e515c9efac3fbda63fa0ed
JMSS-Unknown <31131631+JMSS-Unknown@users.noreply.github.com>
2022-09-14 19:17:32 +1000

    Comment chunk size

--- src/utils/chunkUpload.ts
+++ src/utils/chunkUpload.ts
@@ -1,5 +1,9 @@
 import api from '../api'
 
+// Files above 300 MB return an empty string in Chrome,
+// hence the chunk size is set to 100 MB which is safely
+// under the limit and is large enough to not have an
+// unreasonable number of chunks for large uploads.
 const CHUNK_SIZE = 100 * 1024 * 1024
 
 const chunkUpload = async (fileId: string, file: File) => {
