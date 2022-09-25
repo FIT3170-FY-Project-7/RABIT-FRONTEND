0679019b03dcb12d9058a5054e36b2d2cc2b1188
Daniel-W-230 <dwat005@student.monash.edu>
2022-09-26 09:03:33 +1000

    feature/plotting-qol-updates: Test re-add white space

--- src/content/plots/Appearance/AppearanceConfiguration.tsx
+++ src/content/plots/Appearance/AppearanceConfiguration.tsx
@@ -3,39 +3,39 @@ import { DatasetConfig } from '../PlotTypes'
 import DatasetConfiguration from './DatasetConfiguration'
 
 type AppearanceConfigPropType = {
-  datasets: DatasetConfig[]
-  setDatasets: (datasets: DatasetConfig[]) => void
+    datasets: DatasetConfig[]
+    setDatasets: (datasets: DatasetConfig[]) => void
 }
 
 const AppearanceConfig = ({ datasets, setDatasets }: AppearanceConfigPropType) => {
-  const updateDatasets = (index: number) => dataset => {
-    const new_datasets = [...datasets]
-    new_datasets[index] = dataset
-    setDatasets(new_datasets)
-  }
+    const updateDatasets = (index: number) => dataset => {
+        const new_datasets = [...datasets]
+        new_datasets[index] = dataset
+        setDatasets(new_datasets)
+    }
 
-  const reorderDatasets = (prevIndex: number, nextIndex: number) => {
-    const new_datasets = [...datasets]
-    const tempDataset = new_datasets[prevIndex]
-    new_datasets[prevIndex] = { ...new_datasets[nextIndex] }
-    new_datasets[nextIndex] = { ...tempDataset }
-    setDatasets(new_datasets)
-  }
+    const reorderDatasets = (prevIndex: number, nextIndex: number) => {
+        const new_datasets = [...datasets]
+        const tempDataset = new_datasets[prevIndex]
+        new_datasets[prevIndex] = { ...new_datasets[nextIndex] }
+        new_datasets[nextIndex] = { ...tempDataset }
+        setDatasets(new_datasets)
+    }
 
-  return (
-    <Box>
-      {datasets.map((dataset: DatasetConfig, index: number) => (
-        <DatasetConfiguration
-          key={`dataset-config-${index}`}
-          dataset={dataset}
-          index={index}
-          datasetsLength={datasets.length}
-          reorderCallback={reorderDatasets}
-          updateDatasets={updateDatasets(index)}
-        ></DatasetConfiguration>
-      ))}
-    </Box>
-  )
+    return (
+        <Box>
+            {datasets.map((dataset: DatasetConfig, index: number) => (
+                <DatasetConfiguration
+                    key={`dataset-config-${index}`}
+                    dataset={dataset}
+                    index={index}
+                    datasetsLength={datasets.length}
+                    reorderCallback={reorderDatasets}
+                    updateDatasets={updateDatasets(index)}
+                ></DatasetConfiguration>
+            ))}
+        </Box>
+    )
 }
 
 export default AppearanceConfig
