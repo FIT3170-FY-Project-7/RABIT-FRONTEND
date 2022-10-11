c1b40e36725c7474386ba4bafdd3ec54b9f8f76e
jpit0004 <102277225+cronix38@users.noreply.github.com>
2022-10-11 19:14:11 +1100

    fixed example json format
    Co-authored-by: Teg64 <101340211+Teg64@users.noreply.github.com>

--- src/content/pages/FileUpload/constants.tsx
+++ src/content/pages/FileUpload/constants.tsx
@@ -4,12 +4,12 @@
       attribute1: [0.15186366972638765, -0.796240857285804],
       attribute2: [
         {
-          complex: true,
+          __complex__: true,
           real: 20.274669606394458,
           imag: -1.9407450062971325
         },
         {
-          complex: true,
+          __complex__: true,
           real: 20.36245193237846,
           imag: -1.486144336535883
         }
