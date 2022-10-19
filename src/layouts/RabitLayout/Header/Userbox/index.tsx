5dcb593c669fdaa6b6c287cc3e35881f5b6b6744
Venura <valw0002@student.monash.edu>
2022-10-19 21:58:49 +1100

    Changing profile button to an about button

--- src/layouts/RabitLayout/Header/Userbox/index.tsx
+++ src/layouts/RabitLayout/Header/Userbox/index.tsx
@@ -4,45 +4,46 @@ import ExpandMoreTwoToneIcon from '@mui/icons-material/ExpandMoreTwoTone'
 import InsightsIcon from '@mui/icons-material/Insights'
 import LockOpenTwoToneIcon from '@mui/icons-material/LockOpenTwoTone'
 import {
-    Avatar,
-    Box,
-    Button,
-    Divider,
-    Hidden,
-    lighten,
-    List,
-    ListItem,
-    ListItemText,
-    Popover,
-    Typography
+  Avatar,
+  Box,
+  Button,
+  Divider,
+  Hidden,
+  lighten,
+  List,
+  ListItem,
+  ListItemText,
+  Popover,
+  Typography
 } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import { useRef, useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 
 import AvatarAvi from 'assets/images/avatars/avivajpeyi.png'
 import { useUserContext } from '../../../../content/Auth/UserContext'
 
 const UserBoxButton = styled(Button)(
-    ({ theme }) => `
+  ({ theme }) => `
         padding-left: ${theme.spacing(1)};
         padding-right: ${theme.spacing(1)};`
 )
 
 const MenuUserBox = styled(Box)(
-    ({ theme }) => `
+  ({ theme }) => `
         background: ${theme.colors.alpha.black[5]};
         padding: ${theme.spacing(2)};`
 )
 
 const UserBoxText = styled(Box)(
-    ({ theme }) => `
+  ({ theme }) => `
         text-align: left;
         padding-left: ${theme.spacing(1)};`
 )
 
 const UserBoxLabel = styled(Typography)(
-    ({ theme }) => `
+  ({ theme }) => `
         overflow-wrap: true;
         word-break: break-all;'
         font-weight: ${theme.typography.fontWeightBold};
@@ -51,87 +52,80 @@ const UserBoxLabel = styled(Typography)(
 )
 
 const UserBoxDescription = styled(Typography)(
-    ({ theme }) => `
+  ({ theme }) => `
         color: ${lighten(theme.palette.secondary.main, 0.5)}`
 )
 
 function HeaderUserbox() {
-    const user = {
-        name: 'Avi Vajpeyi',
-        avatar: AvatarAvi,
-        jobtitle: 'Project Manager'
-    }
+  const user = {
+    name: 'Avi Vajpeyi',
+    avatar: AvatarAvi,
+    jobtitle: 'Project Manager'
+  }
 
-    const { logout } = useUserContext()
-    const ref = useRef<any>(null)
-    const [isOpen, setOpen] = useState<boolean>(false)
+  const { logout } = useUserContext()
+  const ref = useRef<any>(null)
+  const [isOpen, setOpen] = useState<boolean>(false)
 
-    const handleOpen = (): void => {
-        setOpen(true)
-    }
+  const handleOpen = (): void => {
+    window.open('https://fit3170-fy-project-7.github.io/RABIT-DOCS/')
+  }
 
-    const handleClose = (): void => {
-        setOpen(false)
-    }
+  const handleClose = (): void => {
+    setOpen(false)
+  }
 
-    return (
-        <>
-            <UserBoxButton color='secondary' ref={ref} onClick={handleOpen}>
-                <Avatar variant='rounded' alt={user.name} src={user.avatar} />
-                <Hidden mdDown>
-                    <UserBoxText>
-                        <UserBoxLabel variant='body1'>{user.name}</UserBoxLabel>
-                    </UserBoxText>
-                </Hidden>
-                <Hidden smDown>
-                    <ExpandMoreTwoToneIcon sx={{ ml: 1 }} />
-                </Hidden>
-            </UserBoxButton>
-            <Popover
-                anchorEl={ref.current}
-                onClose={handleClose}
-                open={isOpen}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                }}
-            >
-                <MenuUserBox sx={{ minWidth: 210 }} display='flex'>
-                    <Avatar variant='rounded' alt={user.name} src={user.avatar} />
-                    <UserBoxText>
-                        <UserBoxLabel variant='body1'>{user.name}</UserBoxLabel>
-                        <UserBoxDescription variant='body2'>{user.jobtitle}</UserBoxDescription>
-                    </UserBoxText>
-                </MenuUserBox>
-                <Divider sx={{ mb: 0 }} />
-                <List sx={{ p: 1 }} component='nav'>
-                    <ListItem button to='/management/profile/details' component={NavLink}>
-                        <AccountBoxTwoToneIcon fontSize='small' />
-                        <ListItemText primary='My Profile' />
-                    </ListItem>
-                    <ListItem button to='/visualise' component={NavLink}>
-                        <InsightsIcon fontSize='small' />
-                        <ListItemText primary='Explore' />
-                    </ListItem>
-                    <ListItem button to='/management/profile/settings' component={NavLink}>
-                        <AccountTreeTwoToneIcon fontSize='small' />
-                        <ListItemText primary='Account Settings' />
-                    </ListItem>
-                </List>
-                <Divider />
-                <Box sx={{ m: 1 }}>
-                    <Button color='primary' onClick={logout} fullWidth>
-                        <LockOpenTwoToneIcon sx={{ mr: 1 }} />
-                        Sign out
-                    </Button>
-                </Box>
-            </Popover>
-        </>
-    )
+  return (
+    <>
+      <UserBoxButton color='secondary' ref={ref} onClick={handleOpen}>
+        <HelpOutlineIcon sx={{ marginRight: '0.5rem' }} />
+        <Typography variant='h6'>About</Typography>
+      </UserBoxButton>
+      <Popover
+        anchorEl={ref.current}
+        onClose={handleClose}
+        open={isOpen}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}
+      >
+        <MenuUserBox sx={{ minWidth: 210 }} display='flex'>
+          <Avatar variant='rounded' alt={user.name} src={user.avatar} />
+          <UserBoxText>
+            <UserBoxLabel variant='body1'>{user.name}</UserBoxLabel>
+            <UserBoxDescription variant='body2'>{user.jobtitle}</UserBoxDescription>
+          </UserBoxText>
+        </MenuUserBox>
+        <Divider sx={{ mb: 0 }} />
+        <List sx={{ p: 1 }} component='nav'>
+          <ListItem button to='/management/profile/details' component={NavLink}>
+            <AccountBoxTwoToneIcon fontSize='small' />
+            <ListItemText primary='My Profile' />
+          </ListItem>
+          <ListItem button to='/visualise' component={NavLink}>
+            <InsightsIcon fontSize='small' />
+            <ListItemText primary='Explore' />
+          </ListItem>
+          <ListItem button to='/management/profile/settings' component={NavLink}>
+            <AccountTreeTwoToneIcon fontSize='small' />
+            <ListItemText primary='Account Settings' />
+          </ListItem>
+        </List>
+        <Divider />
+        <Box sx={{ m: 1 }}>
+          <Button color='primary' onClick={logout} fullWidth>
+            <LockOpenTwoToneIcon sx={{ mr: 1 }} />
+            Sign out
+          </Button>
+        </Box>
+      </Popover>
+    </>
+  )
 }
 
 export default HeaderUserbox
